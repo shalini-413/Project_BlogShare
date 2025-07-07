c6c6e9601350498bc254577cddb844bfda42a205
Shalini <shalinishalu462002@gmail.com>
2025-07-08 00:14:46 +0530

    all logic corrected

--- blogshare-server/routes/authRoutes.js
+++ blogshare-server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware.js").default;
+const auth = require("../middleware/authMiddleware")
 
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -13,14 +13,6 @@ router.post("/register", async (req, res) => {
   res.status(201).json(user);
 });
 
-// router.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email });
-//   if (!user || !(await bcrypt.compare(password, user.password)))
-//     return res.status(400).json({ message: "Invalid credentials" });
-//   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-//   res.json({ token, user });
-// });
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
