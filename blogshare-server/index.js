32e9fb97aed6cabcbcb26bb1632e04c998797865
Shalini <shalinishalu462002@gmail.com>
2025-07-09 14:27:32 +0530

    resolved cors errors

--- blogshare-server/index.js
+++ blogshare-server/index.js
@@ -9,6 +9,13 @@ const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 // app.use(cors());
+
+app.use(cors({
+  origin: "https://project-blog-share.vercel.app",
+  credentials: true            
+}));
+
+
 app.use(express.json());
 app.use(cookieParser());
 
